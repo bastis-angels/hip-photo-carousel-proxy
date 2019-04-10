18c5778d1a62ba065eb944aa7eee56f50115f676
Megan Keesee <hello@megankeesee.com>
2019-04-10 15:33:32 -0700

    add photo carousel and booking modules to proxy

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(morgan('dev'));
+app.use('/listing/:listingID', express.static('public'));
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
--- photo-carousel/server/database/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/Listing';
-
-const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
-
-module.exports = db;
